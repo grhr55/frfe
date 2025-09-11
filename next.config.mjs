833a55caee6f578ccf71e0d91a1ce73759d6a5f2
kali <kali@kali>
2025-09-11 18:38:55 +0300

    Обновление проекта: все изменения

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 images: {
-    domains: ['localhost'], // сюда добавь хост
+    domains: ['fourfeef'], // сюда добавь хост
   },
 };
 export default nextConfig;
