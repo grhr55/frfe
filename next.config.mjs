77f37c22f8a183f18e4af1620471f47b2df298f9
kali <kali@kali>
2025-09-18 17:56:02 +0300

    Обновление проекта: все изменения

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 images: {
-    domains: ['iefhie.onrender.com','localhost'],
+    domains: ['iefhie.onrender.com'],
   },
 };
 export default nextConfig;
