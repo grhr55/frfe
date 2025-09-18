27f4bebcc8f29a23b18432dd3828b12ff701ce5c
kali <kali@kali>
2025-09-18 19:14:31 +0300

    Обновление проекта: все изменения

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
+import Heder from './components/Heder';
+import Seo from './Seo';
 
-import Heder from './components/Heder'
 export default function Home() {
+  const metadata = {
+    title: "IT Fullstack Web – Web & App Development",
+    description:
+      "Full-stack web development: React, Next.js, Node.js, databases, and SEO optimization.",
+    url: "https://portfolio45445.netlify.app/",
+    image: "https://portfolio45445.netlify.app/favicon.ico", 
+    keywords: "IT, Fullstack, React, Next.js, Node.js, SEO",
+  };
+
   return (
-   <div>
-    <Heder/>
-    
-  
-   </div>
+    <div>
+      <Seo {...metadata} />
+      <Heder />
+    </div>
   );
 }
